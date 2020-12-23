99212c0f58e09194a76a5840677afa1bfe158433
Prabhudev S Betsurmath <prabhudevbetsurmath@gmail.com>
2020-12-24 01:01:48 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+setInterval(() => {
+    
+
+theDate = new Date;
+theday = theDate.getDay();
+today = theDate.getDate()+"/"+theDate.getMonth()+"/"+theDate.getFullYear();
+
+hours = theDate.getHours();
+min = theDate.getMinutes();
+seconds = theDate.getSeconds();
+time = hours+12 +":"+ min + ":" + seconds  
+c=["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+for(var i=0;i<7;i++){
+if (theday==i){
+    theday=c[i]
+    
+}
+}
+
+document.getElementById("time").innerHTML = theday +" "+ today +" " + time;
+}, 1000);
